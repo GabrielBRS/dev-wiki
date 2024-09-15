93e27cad406baec4fb7d68e00ac2889e2e1b8d02
gabri <gabriel.brs.gsousa@gmail.com>
2024-09-15 19:32:30 -0300

    frist

---
+++ docusaurus.config.js
@@ -0,0 +1,167 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Dev Wiki',
+  tagline: 'Aprenda T.I sem complicação',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/GabrielBRS',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/GabrielBRS',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Dev Wiki',
+        logo: {
+          alt: 'Dev Wiki Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Docs',
+          },
+          // {
+          //   type: 'guideSidebar',
+          //   sidebarId: 'guidesSidebar',
+          //   position: 'left',
+          //   label: 'Guides',
+          // },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/GabrielBRS',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Tecnologia da Informação',
+            items: [
+              {
+                label: 'Doc',
+                to: '/docs/intro',
+              },
+              // {
+              //   label: 'Guia',
+              //   to: '/guides/intro',
+              // },
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+            ],
+          },
+          {
+            title: 'Comunidade',
+            items: [
+              {
+                label: 'QConcursos',
+                href: 'https://www.qconcursos.com/usuario/perfil/concurseiro_f7d90298',
+              },
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/in/enggabrielbarros/',
+              },
+              {
+                label: 'Youtube',
+                href: 'https://www.youtube.com/@devgabrielsousa',
+              },
+              {
+                label: 'Instagram',
+                href: 'https://www.instagram.com/devgabrielsousa/',
+              },
+            ],
+          },
+          {
+            title: 'Repositório',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/GabrielBRS',
+              },
+              {
+                label: 'Site Pessoal',
+                href: 'https://www.youtube.com/@devgabrielsousa',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Inc. construído com Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
